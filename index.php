8bae7227291791d173f1d9e1822a6dc8c300341e
neishilasekarsari <161546732+neishilasekarsari@users.noreply.github.com>
2024-02-28 10:17:41 +0700

    index
    index-UKKgaleri

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/index.css">
+    <title>Novict</title>
+</head>
+<body>
+    <main>
+        <div class="Banner">
+            <h1></h1>
+        </div>
+        <div class="Container">
+            <h2>Selamat Datang</h2>
+            <div class="InnerContainer">
+                <a href="login.php" class="Login">Login</a>
+                <a href="register.php" class="Daftar">Daftar</a>
+            </div>
+        </div>
+    </main>
+    <script src="asset/js/script.js"></script>
+</body>
+</html>
